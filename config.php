b683b6a23bfd6a18610e0f9dc8a3543f9abaf848
MunozLopezKelvin <90059668+MunozLopezKelvin@users.noreply.github.com>
2023-09-19 21:21:38 -0500

    update

--- config.php
+++ config.php
@@ -1,11 +1,18 @@
 <?php
 date_default_timezone_set('America/Lima');
 session_start();
-define('RUTA', 'http://localhost/shop/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+
+// define('RUTA', 'http://localhost/ecomerce-main/');
+
+define('DB_HOST', 'ecomerce.mysql.database.azure.com');
+define('DB_USER', 'kpaulml');
 define('DB_NAME', 'shop');
-define('DB_PASS', '');
+define('DB_PASS', '@KPMLadm001');
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_NAME', 'shop');
+// define('DB_PASS', '');
 
 define('CLIENT_ID_PAYPAL', 'ARtELM9gW5sQcYEDb-11-jdoPAig6Y--f9Ggu9-RStP0QO4QrECMBHzzQaownQd1lZCL4yoJEOmYo8CN');
 ?>
