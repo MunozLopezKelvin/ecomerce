775eab073fe5f55da283b2bbbfb833122c853b79
MunozLopezKelvin <90059668+MunozLopezKelvin@users.noreply.github.com>
2023-09-24 22:34:19 -0500

    a

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Lima');
 session_start();
 
 // define('RUTA', 'http://localhost/ecomerce-main/');
-define('RUTA', 'ecommerce-nube.azurewebsites.net/');
+define('RUTA', 'https://ecommerce-nube.azurewebsites.net/');
 
 define('DB_HOST', 'ecomerce.mysql.database.azure.com');
 define('DB_USER', 'kpaulml');
